73a4752d9b713305eda1ea33273ebf28a54eb6fa
Krzysztof Langner <klangner@gmail.com>
2012-12-01 19:55:37 +0100

    new bulk loader

--- src/matrobot/project/models.py
+++ src/matrobot/project/models.py
@@ -7,13 +7,11 @@ Created on 2012-12-01
 
 from google.appengine.ext import db
 
-
 class ProjectActivity(db.Model):
     name = db.StringProperty()
     year = db.IntegerProperty()
     month = db.IntegerProperty()
     push_count = db.IntegerProperty()
 
-
-def project_activity_key(name):
-    return db.Key.from_path('ProjectActivity', name)
+#def project_activity_key(name):
+#    return db.Key.from_path('Project', name)
--- src/matrobot/project/views.py
+++ src/matrobot/project/views.py
@@ -5,15 +5,13 @@ Created on 2012-12-01
 @author: Krzysztof Langner
 '''
 from django.shortcuts import render_to_response
-from matrobot.project.models import ProjectActivity, project_activity_key
+from matrobot.project.models import ProjectActivity
 
 
 def index(request):
     name = request.GET.get('name', '')
     if name:
-        projects = ProjectActivity.gql("WHERE ANCESTOR IS :1 AND name=:2",
-                                   project_activity_key(name),
-                                   name)
+        projects = ProjectActivity.gql("WHERE name=:1", name)
         if projects.count(1) > 0:
             return render_to_response('project/index.html', {'name':name})
         
