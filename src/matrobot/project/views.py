0969e8400d4f2f0c943102422301e2b3d875e0ba
Krzysztof Langner <klangner@gmail.com>
2012-12-01 16:25:11 +0100

    bulkloader

--- src/matrobot/project/views.py
+++ src/matrobot/project/views.py
@@ -9,13 +9,18 @@ from matrobot.project.models import ProjectActivity, project_activity_key
 
 
 def index(request):
-    name = request.GET.get('name', '')
-    projects = ProjectActivity.gql("WHERE ANCESTOR IS :1 AND name=:2",
-                                   project_activity_key(name),
-                                   name)
+    name = request.GET.get('name', 'noname')
+#    projects = ProjectActivity.gql("WHERE ANCESTOR IS :1 AND name=:2",
+#                                   project_activity_key(name),
+#                                   name)
+#    if projects.count(1) == 0:
+#        return render_to_response('project/not_found.html', {'name':name})
+    
+    project = ProjectActivity(parent=project_activity_key(name))
+    project.name = name
+    project.year = 2011
+    project.month = 11
+    project.push_count = 13
+    project.put()
 
-    
-    if projects.count(1) == 0:
-        return render_to_response('project/not_found.html', {'name':name})
-    
     return render_to_response('project/index.html', {'name':name})
