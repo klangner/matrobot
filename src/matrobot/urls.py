086c1120dc17285bb227db5a7b3b97568fd0c6d4
Krzysztof Langner <klangner@gmail.com>
2012-12-28 12:13:24 +0100

    Static files served by GAE instead of django

--- src/matrobot/urls.py
+++ src/matrobot/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls.defaults import patterns, include, url
-import os
 
 urlpatterns = patterns('',
     url(r'^$', 'matrobot.public.views.index'),
     (r'^people/', include('matrobot.people.urls')),    
     (r'^project/', include('matrobot.project.urls')),    
     (r'^public/', include('matrobot.public.urls')),    
-
-    # System URLs
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': os.path.join(os.path.dirname(__file__), 'templates/media/')}),
 )
